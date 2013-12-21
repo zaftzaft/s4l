21dda652e66136db024ebf86e1c80ace13536552
zaftzaft <zaftzaft7@gmail.com>
2013-12-21 14:10:10 +0900

    fix line comment bug.

--- s4l.js
+++ s4l.js
@@ -1,6 +1,6 @@
 /*
  * Sort the CSS properties for LESS
- * 2013/12/19
+ * 2013/12/21
  */
 var properties = require("./properties.js");
 
@@ -26,7 +26,7 @@ module.exports = (function(){
         return stack(b);
       }
       else{
-        return stack(sort(b), true);
+        return stack(s4l.sort(b), true);
       }
     });
   };
@@ -49,7 +49,7 @@ module.exports = (function(){
     return re;
   };
 
-  var sort = s4l.sort = function(str){
+  s4l.sort = function(str){
     var line = str.split(";");
     var count = 1;
     var len = properties.length;
@@ -58,28 +58,45 @@ module.exports = (function(){
       var index = properties.indexOf(prop);
       return ~index ? index : (len + count++);
     };
+
+    /* "prop: value; // hoge" */
+    var rlc = /\/\/(.*)\n.+:/;
+
+    // Line Comment Only
+    var rlcOnly = /\n\/\/(.*)\n.+:/;
+
+    for(var i = 0,l = line.length;i < l;i++){
+      if(!rlcOnly.test(line[i]) && rlc.test(line[i])){
+        var sp = line[i].split("\n");
+        line[i-1] += sp.shift();
+        line[i] = "\n" + sp.join("\n");
+      }
+      line[i] += ";";
+    }
+
     return line.map(function(style){
       var pos;
-      var prop = style.split(":")[0].trim();
-      // Variable
+      var sp = style.split(":");
+      var prop = sp[0].trim();
+      var value = sp[1];
+      // Variable (@prop: value;)
       if(prop[0] === "@"){
         pos = -1;
       }
-      // IE7
+      // IE7 (*prop: value;)
       else if(prop[0] === "*"){
         pos = getPos(prop.slice(1)) + 0.1;
       }
+      // Style (prop: value;)
       else {
         pos = getPos(prop);
       }
-      return [
-        style, pos
-      ];
+      return [style, pos];
     })
     .sort(function(a, b){
       return a[1] > b[1] ? 1 : -1;
     }).map(function(a){
-      return a[0] + ";";
+      return a[0];
     }).join("") + end;
   };
 
