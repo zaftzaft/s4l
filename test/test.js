b2405f2023423fa946094da4797110274cd8b6c1
zaftzaft <zaftzaft7@gmail.com>
2013-12-21 14:11:59 +0900

    2nd Commit!!!!!!!!!!!

--- test/test.js
+++ test/test.js
@@ -1,12 +1,14 @@
-var s4l = require("../s4l.js");
+// For debug
+var s4l = require("../s4l");
 var fs = require("fs");
-
 var test = function(t, n){
   for(;n--;){
     t = s4l.findBlock(t);
   }
   return t;
 }
-var t = fs.readFileSync("./7.less", "utf8");
+
+var t = fs.readFileSync("./10.less", "utf8");
+//console.log(test(t,1));
 console.log(s4l(t));
 console.log(s4l.styles);
